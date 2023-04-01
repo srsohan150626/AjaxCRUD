2d20f9432a3353092bfc63ac00b6ce2bdda1f808
Md Sohanur Rahaman <sohan.ice.pust@gmail.com>
2023-04-01 23:01:55 +0600

    itextsharp add

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.Metrics;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.text.html.simpleparser;
 
 namespace AdvanceAjaxCRUD.Controllers
 {
@@ -135,6 +140,20 @@ namespace AdvanceAjaxCRUD.Controllers
             lstCountries.Insert(0, defItem);
 
             return Json(lstCountries);
+        }
+        [HttpPost]
+        public FileResult Export(string GridHtml)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                StringReader sr = new StringReader(GridHtml);
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf", "Grid.pdf");
+            }
         }
 
     }
