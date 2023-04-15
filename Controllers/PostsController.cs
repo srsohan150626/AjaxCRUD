db7f6b7fd199bb223641e16e1f8ae4d108f64977
Md Sohanur Rahaman <sohan.ice.pust@gmail.com>
2023-04-15 14:04:29 +0600

    itext 7 integration for  pdf

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AdvanceAjaxCRUD.Data;
 using AdvanceAjaxCRUD.Models;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Html2pdf;
+using iText.Layout.Element;
+using iText.Html2pdf.Resolver.Font;
+using AdvanceAjaxCRUD.Helper;
+using iText.Kernel.Events;
 
 namespace AdvanceAjaxCRUD.Controllers
 {
@@ -22,11 +29,9 @@ namespace AdvanceAjaxCRUD.Controllers
         // GET: Posts
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Post.ToListAsync());
+            return View(await _context.Post.ToListAsync());
         }
-
-        // GET: Posts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> GeneratePdf(int? id)
         {
             if (id == null || _context.Post == null)
             {
@@ -35,127 +40,164 @@ namespace AdvanceAjaxCRUD.Controllers
 
             var post = await _context.Post
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (post == null)
-            {
-                return NotFound();
-            }
+            var title = post.Title;
+            var content = post.Content;
+            var Id = post.Id;
+            // Concatenate the title and content into a single HTML string
+            var htmlContent = $"<p>{title}</p>{content}";
+            var header = "this is short header";
 
-            return View(post);
+            var memoryStream = new MemoryStream();
+
+            var writer = new PdfWriter(memoryStream);
+            var pdfDocument = new PdfDocument(writer);
+            var document = new Document(pdfDocument);
+            // Set up the header handler
+            // Add the header event handler to the pdf document
+            pdfDocument.AddEventHandler(PdfDocumentEvent.START_PAGE, new HeaderEventHandler(header));
+
+            HtmlConverter.ConvertToPdf(htmlContent, writer, new ConverterProperties());
+
+            //pdfDocument.Close();
+            //document.Close();
+
+            return File(memoryStream.ToArray(), "application/pdf", "memo.pdf");
         }
 
-        // GET: Posts/Create
-        public IActionResult Create()
+
+
+    // GET: Posts/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null || _context.Post == null)
         {
-            return View();
+            return NotFound();
         }
 
-        // POST: Posts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Content")] Post post)
+        var post = await _context.Post
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (post == null)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(post);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(post);
+            return NotFound();
         }
 
-        // GET: Posts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        return View(post);
+    }
+
+    // GET: Posts/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Posts/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,Content")] Post post)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null || _context.Post == null)
-            {
-                return NotFound();
-            }
-
-            var post = await _context.Post.FindAsync(id);
-            if (post == null)
-            {
-                return NotFound();
-            }
-            return View(post);
-        }
-
-        // POST: Posts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content")] Post post)
-        {
-            if (id != post.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(post);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PostExists(post.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(post);
-        }
-
-        // GET: Posts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Post == null)
-            {
-                return NotFound();
-            }
-
-            var post = await _context.Post
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (post == null)
-            {
-                return NotFound();
-            }
-
-            return View(post);
-        }
-
-        // POST: Posts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Post == null)
-            {
-                return Problem("Entity set 'AppDbContext.Post'  is null.");
-            }
-            var post = await _context.Post.FindAsync(id);
-            if (post != null)
-            {
-                _context.Post.Remove(post);
-            }
-            
+            _context.Add(post);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-        private bool PostExists(int id)
-        {
-          return _context.Post.Any(e => e.Id == id);
-        }
+        return View(post);
     }
+
+    // GET: Posts/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || _context.Post == null)
+        {
+            return NotFound();
+        }
+
+        var post = await _context.Post.FindAsync(id);
+        if (post == null)
+        {
+            return NotFound();
+        }
+        return View(post);
+    }
+
+    // POST: Posts/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content")] Post post)
+    {
+        if (id != post.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(post);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(post.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(post);
+    }
+
+    // GET: Posts/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null || _context.Post == null)
+        {
+            return NotFound();
+        }
+
+        var post = await _context.Post
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        return View(post);
+    }
+
+    // POST: Posts/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        if (_context.Post == null)
+        {
+            return Problem("Entity set 'AppDbContext.Post'  is null.");
+        }
+        var post = await _context.Post.FindAsync(id);
+        if (post != null)
+        {
+            _context.Post.Remove(post);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool PostExists(int id)
+    {
+        return _context.Post.Any(e => e.Id == id);
+    }
+    
+}
 }
